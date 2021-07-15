183551951e815eb68014a80b552f8cc52000d5e4
ricky <shulakna@gmail.com>
2021-07-15 16:29:03 +0300

    +

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,10 @@
 module.exports = {
     getUsers: async (req, res) => {
         const response = await fetch("/api/users", {
-            method: "GET",
-            headers: {"Accept": "application/json"}
+            method: "GET"
         });
 
         if (response.ok === true) {
-            console.log('ok')
             const users = await response.json();
             res.json(users);
         }
