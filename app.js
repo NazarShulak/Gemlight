baa369370a54c9ed494e15356770b80ff346feda
ricky <shulakna@gmail.com>
2021-07-16 14:33:43 +0300

    swagger

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 
 const { userRouter } = require('./routes');
 const { constants } = require('./constants');
@@ -8,6 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'API users',
+            description: 'API Information',
+        },
+
+        servers: ['http://localhost:8000']
+    },
+
+    apis: ['.routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/', userRouter);
 
 app.listen(constants.PORT, () => {
