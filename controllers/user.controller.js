05347be89155964329af51dd0efacb34835184fd
ricky <shulakna@gmail.com>
2021-07-16 13:14:39 +0300

    +

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 module.exports = {
     getUser: async (req, res, next) => {
         try {
-            await fetch('https://dev.api.cloud.picupmedia.com/docs#/User/')
+            await fetch('https://dev.api.cloud.picupmedia.com/docs#/User/get_api_users_me')
                 .then(res => res.json())
                 .then(json => res.json(json));
 
