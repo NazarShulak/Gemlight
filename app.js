01be021221023bd9e9909953c6ed0c8a74b18ef5
ricky <shulakna@gmail.com>
2021-07-16 11:31:40 +0300

    +

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', userRouter);
+// app.get('https://dev.api.cloud.picupmedia.com/docs#/User/get_api_users_me', (req, res) => {
+//     res.json('Welcome');
+// })
 
-app.listen(constants.PORT, () => {
+app.get('/', (req, res) => {
+    res.json('Welcome');
+})
+
+app.listen(process.env.PORT || constants.PORT, () => {
     console.log('app listen 8000');
 });
--- router/user-router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const {userController: {getUsers}} = require('../controller');
-
-router.get('/users', getUsers);
-
-module.exports = router;
