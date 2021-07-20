b769bc211d9b0efad7576a193c637932d32d1dac
ricky <shulakna@gmail.com>
2021-07-20 11:31:55 +0300

    +

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,8 @@ const Sequelize = require('sequelize');
 
 const { constants: { DB_PASSWORD, DB_CONNECTION_STRING, DB_NAME, DB_USER } } = require('../constants');
 
-<<<<<<< HEAD
-const sequelize = new Sequelize('users', 'admin', 'superadmin', {
-    host: 'api-database.cvhms5uyrffc.us-east-2.rds.amazonaws.com',
-=======
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_CONNECTION_STRING,
->>>>>>> 830434f2696aa42d68db2683afcb39d23672bd86
     dialect: 'mysql',
     logging: false
 });
