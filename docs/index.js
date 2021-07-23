7f6be57ab92d5128c92122e72f222cba886d11bd
ricky <shulakna@gmail.com>
2021-07-23 10:54:09 +0300

    +

--- docs/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.components = require('./components');
-module.exports.info = require('./info');
-module.exports.servers = require('./servers');
-module.exports.product = require('./product');
-module.exports.user = require('./user');
