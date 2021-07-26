649f66bf1060b7d65085ceaefb5267243f900891
Ubuntu <ubuntu@ip-172-31-3-24.us-east-2.compute.internal>
2021-07-26 12:58:52 +0000

    +

--- database/connection.js
+++ database/connection.js
@@ -2,10 +2,11 @@ const Sequelize = require('sequelize');
 
 const { constants: { DB_PASSWORD, DB_CONNECTION_STRING, DB_NAME, DB_USER } } = require('../constants');
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_CONNECTION_STRING,
+const sequelize = new Sequelize('users','admin', 'superadmin', {
+    host: 'api-database.cvhms5uyrffc.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false
 });
 
 module.exports = sequelize;
+global.sequelize = sequelize;
