c4f6ff87cf1f5aec34acfccfea32f9cd6548780b
ricky <shulakna@gmail.com>
2021-07-28 15:14:44 +0300

    +

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const delExpiredTokens = require('./delExpiredTokens');
 
 module.exports.cronRun = async () => {
-    cron.schedule('*/30 * * * * *', async () => {
+    cron.schedule('* * * * *', async () => {
         console.log('started');
         await delExpiredTokens();
         console.log('finished');
