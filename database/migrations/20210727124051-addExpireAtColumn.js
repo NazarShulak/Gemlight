38815c9bee1199cdd344eb8bbfc4e7105cc3beff
ricky <shulakna@gmail.com>
2021-07-28 10:19:40 +0300

    +

--- database/migrations/20210727124051-addExpireAtColumn.js
+++ database/migrations/20210727124051-addExpireAtColumn.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('userAuth', 'expireAt', { type: Sequelize.DATE, allowNull: false });
+    return queryInterface.addColumn('userAuth', 'expireAt', { type: Sequelize.DATEONLY, allowNull: false });
   },
 
   down: async (queryInterface, Sequelize) => {
