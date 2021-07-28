9201d2e213984c570eee4c19c45a7a1b51f52a02
ricky <shulakna@gmail.com>
2021-07-28 14:57:40 +0300

    +

--- cron-jobs/delExpiredTokens.js
+++ cron-jobs/delExpiredTokens.js
@@ -1,12 +1,13 @@
 const { AuthModel } = require('../database');
 
 module.exports = async () => {
-    let currentDate = new Date()
-    currentDate = currentDate.format("yyyy-mm-dd");
+    const currentDate = new Date()
+    const date = currentDate.format("yyyy-mm-dd");
+
 
     console.log(currentDate);
     await AuthModel.destroy({
-        where: { expireAt: { [Op.lt]: currentDate.getDate() } }
+        where: { expireAt: { [Op.lt]: date } }
     });
     console.log('***SUCCESS****');
 };
