dc2905dceba3c7bce7f27c422bd20810ea0b28ee
ricky <shulakna@gmail.com>
2021-07-29 11:58:17 +0300

    +

--- routes/__test__/user-router.test.js
+++ routes/__test__/user-router.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../../app");
+const { not } = require("joi");
 
 
 describe("GET /api/users ", () => {
@@ -8,12 +9,40 @@ describe("GET /api/users ", () => {
         expect(response.body).toEqual(
             expect.arrayContaining([
                 expect.objectContaining({
-                    user_id: 1,
-                    name:'test',
-                    age:23,
-                    email:'test@add.co'
+                    // user_id: 1,
+                    // name: 'test',
+                    // age: 23,
+                    // email: 'test@add.co'
                 })]))
         expect(response.statusCode).toBe(200);
+    });
+});
+
+describe("POST /api/users ", () => {
+
+    describe('given data is correct', () => {
+        test('It should return user object with status of 200', async () => {
+            const response = await request(app).post("/api/users").send({
+                name: 'jest-test',
+                age: 20,
+                email: 'jest@test.co',
+                password: 'jest12345'
+            });
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toHaveProperty(['user_id', 'name', 'age', 'email', 'password']);
+        });
+    });
+
+    describe('missing some input data', async () => {
+        test('Should respond with status code of 400', async () => {
+            const response = await request(app).post("/api/users").send({
+                name: 'jest-test',
+                age: 20,
+            });
+
+            expect(expect(response.statusCode).toBe(400));
+        });
     });
 });
 
