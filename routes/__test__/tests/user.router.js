26425dc7ee11db21808c5780c60f66fbf69ecc37
ricky <shulakna@gmail.com>
2021-07-30 12:10:10 +0300

    +

--- routes/__test__/tests/user.router.js
+++ routes/__test__/tests/user.router.js
@@ -71,13 +71,17 @@ module.exports = () => {
             test("It should respond with an array of users", async () => {
                 const response = await request(app).get("/api/users");
 
-                expect(response.body).toMatchSnapshot([{
-                    user_id: expect.any(Number),
-                    name: expect.any(String),
-                    age: expect.any(Number),
-                    email: expect.any(String),
-                    password: expect.any(String)
-                }]);
+                expect(response.body).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            user_id: expect.any(Number),
+                            name: expect.any(String),
+                            age: expect.any(Number),
+                            email: expect.any(String),
+                            password: expect.any(String)
+                        })
+                    ])
+                );
 
                 expect(response.statusCode).toBe(200);
             });
