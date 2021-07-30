7af00c8f3f524c2275d984976746b587115f7739
ricky <shulakna@gmail.com>
2021-07-30 12:03:31 +0300

    +

--- routes/__test__/tests/user.router.js
+++ routes/__test__/tests/user.router.js
@@ -15,49 +15,65 @@ module.exports = () => {
                 });
 
                 expect(response.statusCode).toBe(201);
-            });
-        });
-
-        describe('When given data is duplicate', () => {
-            test('Should respond with status code of 409', async () => {
-                const response = await request(app).post("/api/users").send({
+                expect(response.body).toMatchSnapshot({
+                    user_id: expect.any(Number),
                     name: 'test',
                     age: 20,
                     email: 'test@test.co',
-                    password: 'test12345'
+                    password: expect.any(String)
                 });
+            });
 
-                expect(response.statusCode).toBe(409);
+            describe('When given data is duplicate', () => {
+                test('Should respond with status code of 409', async () => {
+                    const response = await request(app).post("/api/users").send({
+                        name: 'test',
+                        age: 20,
+                        email: 'test@test.co',
+                        password: 'test12345'
+                    });
+
+                    expect(response.statusCode).toBe(409);
+                });
+            });
+
+            describe('missing some input data', () => {
+                test('Should respond with status code of 500', async () => {
+                    const response = await request(app).post("/api/users").send({
+                        name: 'test',
+                        age: 20,
+                    });
+
+                    expect(expect(response.statusCode).toBe(500));
+                });
             });
         });
 
-        describe('missing some input data', () => {
-            test('Should respond with status code of 500', async () => {
-                const response = await request(app).post("/api/users").send({
-                    name: 'test',
-                    age: 20,
-                });
+        describe("GET /api/users ", () => {
+            test("It should respond with an array of users", async () => {
+                const response = await request(app).get("/api/users");
 
-                expect(expect(response.statusCode).toBe(500));
+                expect(response.body).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({}).toMatchSnapshot({
+                            user_id: expect.any(Number),
+                            name: expect.any(String),
+                            age: expect.any(Number),
+                            email: expect.any(String),
+                            password: expect.any(String)
+                        })
+                    ]));
+
+                expect(response.statusCode).toBe(200);
             });
         });
-    });
 
-    describe("GET /api/users ", () => {
-        test("It should respond with an array of users", async () => {
-            const response = await request(app).get("/api/users");
-
-            expect(response.body).toEqual(
-                expect.arrayContaining([]));
-            expect(response.statusCode).toBe(200);
-        });
-    });
-
-    // describe("DELETE /api/users/:user_id ", () => {
-    //     test("It should respond with status code of 204", async () => {
-    //         const response = await request(app).delete("/api/users/1").set('Authorization', '');
-    //
-    //         expect(response.statusCode).toBe(204);
-    //     });
-    // });
-};
+        // describe("DELETE /api/users/:user_id ", () => {
+        //     test("It should respond with status code of 204", async () => {
+        //         const response = await request(app).delete("/api/users/1").set('Authorization', '');
+        //
+        //         expect(response.statusCode).toBe(204);
+        //     });
+        // });
+    })
+}
