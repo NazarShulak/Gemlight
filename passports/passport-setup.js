43bd3b9f9fd4253b05347ed473dc7ae1e04c6296
ricky <shulakna@gmail.com>
2021-08-09 12:41:24 +0300

    +

--- passports/passport-setup.js
+++ passports/passport-setup.js
@@ -3,19 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { constants: { CLIENT_ID, CLIENT_SECRET } } = require('../constants');
 
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    User.findById(id).then((user) => {
-        done(null, user);
-    });
-});
-
-passport.use(
-    new GoogleStrategy({
-
+passport.use(new GoogleStrategy({
         clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         callbackURL: '/auth/google/redirect'
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -12,15 +12,15 @@ const {
 
 router.post('/local', userBodyCheck, checkUserLogin, checkUserPasswordValidity, loginUser);
 
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
 }));
 
-// router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-//
-//     res.send(req.user);
-//     res.redirect('/profile');
-// });
+router.get('/auth/google/callback', passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+));
 
 
 module.exports = router;
