56c6039f497f60a3a719ddac3fde1e7f14c2656e
ricky <shulakna@gmail.com>
2021-08-09 18:04:24 +0300

    +

--- passports/passport.js
+++ passports/passport.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GoogleStrategy({
         clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
-        callbackURL: 'http://3.142.164.171:8000/auth/google/callback'
+        callbackURL: 'http://localhost:8000/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
         /*
