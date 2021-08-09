6820afc7e806930f903bf04a270e34b02ed8274b
ricky <shulakna@gmail.com>
2021-08-09 12:34:17 +0300

    +

--- passports/passport-setup.js
+++ passports/passport-setup.js
@@ -3,12 +3,22 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { constants: { CLIENT_ID, CLIENT_SECRET } } = require('../constants');
 
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
 
         clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
-        callbackURL: '/auth/local/google/redirect'
+        callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db
         console.log(profile);
