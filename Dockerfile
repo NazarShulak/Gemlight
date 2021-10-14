8ef80b41163ec8fc0ff9fe5737dc7964883a7e86
NazarShulak <shulakna@gmail.com>
2021-10-14 10:44:37 +0300

    +

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,3 @@ COPY ./backend/package.json /app
 
 RUN npm install
 
-COPY wait-for-it.sh /wait-for-it.sh
-RUN chmod +x /wait-for-it.sh
--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,24 +1,15 @@
 const Sequelize = require('sequelize');
-// const asyncRedis = require('async-redis');
 
 const { constants: { DB_PASSWORD, DB_CONNECTION_STRING, DB_NAME, DB_USER } } = require('../constants');
 
-// const client = asyncRedis.createClient();
 
 const sequelize = new Sequelize(DB_NAME+'', DB_USER+'', DB_PASSWORD+'', {
     host: DB_CONNECTION_STRING+'',
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require:true,
-            rejectUnauthorized: false
-        }
-    },
     logging: false
 });
 
 module.exports = {
-    // asyncRedis: client,
     sequelize
 };
